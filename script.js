1b62b38aa28023a6fee7130182f168290de5be7d
Neo19867 <2037356@gmail.com>
2024-08-29 22:03:47 +0700

    Auto-commit: 2024-08-29 22:03:47

--- script.js
+++ script.js
@@ -72,22 +72,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
-    // Проверяем инициализацию Telegram Web App SDK
-    if (window.Telegram && window.Telegram.WebApp) {
-        // Получаем userId из Telegram Web App
-        const userId = window.Telegram.WebApp.initDataUnsafe?.user?.id;
-        const name = window.Telegram.WebApp.initDataUnsafe?.user?.first_name;
-
-        console.log("User ID from Telegram:", userId, name); // Выводим userId в консоль для отладки
-
-        // Проверка наличия userId
-        if (!userId) {
-            console.log("User ID not found, redirecting to bot...");
-            window.location.replace("https://t.me/MrROBOT_helper_bot");
-            return;
-        }
-
-
     const gameOptions = document.querySelectorAll('.game-option');
     const keyCountGroup = document.getElementById('keyCountGroup');
     const keyRange = document.getElementById('keyRange');
@@ -136,12 +120,8 @@ document.addEventListener('DOMContentLoaded', () => {
         // Hide the form sections
         document.querySelector('.grid-container').style.display = 'none';
         keyCountGroup.style.display = 'none';
-        
-        // Найдите элемент с id 'keyCountLabel'
-        let keyCountLabel = document.getElementById('keyCountLabel');
 
-        // Измените текстовое содержимое элемента
-        keyCountLabel.innerText = `Trying to get ${keyCount} keys, ${name}`;
+        keyCountLabel.innerText = `Trying to get ${keyCount} keys`;
 
         progressBar.style.width = '0%';
         progressText.innerText = '0%';
