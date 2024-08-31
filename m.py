ee90d5a0b23c38e02a3da46f8ddec77bf123f67b
Neo19867 <2037356@gmail.com>
2024-08-31 10:47:22 +0700

    Auto-commit: 2024-08-31 10:47:22

--- m.py
+++ m.py
@@ -145,7 +145,7 @@ async def check_all_users():
 async def handle_commands(message: types.Message):
     user_id = message.from_user.id
 
-    if message.text and message.text.lower() == "черный экран / black screen":
+    if message.text and message.text.lower() == "почему не работает / why does not work":
         await message.answer(
         "⚠️Если у вас черный экран и вы не можете получить ваши безлимитные ключи, просто используйте VPN.\nНапример для андроид: VPN (https://play.google.com/store/apps/details?id=com.free.max.speedy.pro.app)\n\n⚠️If you have a black screen and can't get your unlimited keys, just use a VPN.\nFor example for Android: VPN (https://play.google.com/store/apps/details?id=com.free.max.speedy.pro.app)",
         disable_web_page_preview=True
@@ -173,7 +173,7 @@ async def handle_commands(message: types.Message):
         reply_kb = ReplyKeyboardMarkup(
             keyboard=[
                 [KeyboardButton(text="Проверить доступ / Check access")],
-                [KeyboardButton(text="Черный экран / Black screen")]
+                [KeyboardButton(text="почему не работает / why does not work")]
             ],
             resize_keyboard=True
         )
@@ -187,7 +187,7 @@ async def handle_commands(message: types.Message):
         reply_kb = ReplyKeyboardMarkup(
             keyboard=[
                 [KeyboardButton(text="Проверить доступ / Check access")],
-                [KeyboardButton(text="Черный экран / Black screen")]
+                [KeyboardButton(text="почему не работает / why does not work")]
             ],
             resize_keyboard=True
         )
