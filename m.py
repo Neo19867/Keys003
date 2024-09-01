ac14dfab10e95b2176f684cd9168d60190bb4b21
Neo19867 <2037356@gmail.com>
2024-09-01 10:11:24 +0700

    Auto-commit: 2024-09-01 10:11:24

--- m.py
+++ m.py
@@ -145,7 +145,7 @@ async def check_all_users():
 async def handle_commands(message: types.Message):
     user_id = message.from_user.id
 
-    if message.text and message.text.lower() == "это безопасно? / is it safe&":
+    if message.text and message.text.lower() == "это безопасно? / is it safe?":
         await message.answer(
         "⚠️Это совершенно безопасно. Вы получаете ключи со своего еп адреса и со своего устройства.\nВ отличие от других ботов, которые получают ключи на своих устройствах тысячами, здесь вы получите именно те ключи, которые вам бы выдала сама игра.\n\n⚠️It's completely safe. You get keys from your e-mail address and from your device.\nUnlike other bots that get keys on their devices in thousands, here you will get exactly the keys that the game itself would give you.",
         disable_web_page_preview=True
@@ -173,7 +173,7 @@ async def handle_commands(message: types.Message):
         reply_kb = ReplyKeyboardMarkup(
             keyboard=[
                 [KeyboardButton(text="Проверить доступ / Check access")],
-                [KeyboardButton(text="почему не работает / why does not work")]
+                [KeyboardButton(text="это безопасно? / is it safe?")]
             ],
             resize_keyboard=True
         )
@@ -187,7 +187,7 @@ async def handle_commands(message: types.Message):
         reply_kb = ReplyKeyboardMarkup(
             keyboard=[
                 [KeyboardButton(text="Проверить доступ / Check access")],
-                [KeyboardButton(text="почему не работает / why does not work")]
+                [KeyboardButton(text="это безопасно? / is it safe?")]
             ],
             resize_keyboard=True
         )
