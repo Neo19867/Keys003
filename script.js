b8a79556b53643c7b34d04838bb04dfbb071dff5
Neo19867 <2037356@gmail.com>
2024-09-03 20:52:44 +0700

    Auto-commit: 2024-09-03 20:52:44

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ document.addEventListener('DOMContentLoaded', () => {
             attempts: 20,
         },
         12: {
-            name: 'Gangs Wars2',
+            name: 'Gangs Wars',
             appToken: 'b6de60a0-e030-48bb-a551-548372493523',
             promoId: 'c7821fa7-6632-482c-9635-2bd5798585f9',
             timing: 40000, // 20 seconds
