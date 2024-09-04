45ea3c7a0c4385e6ad75aad2291bfe9d1d54acf8
Neo19867 <2037356@gmail.com>
2024-09-04 09:33:09 +0700

    Auto-commit: 2024-09-04 09:33:09

--- m.py
+++ m.py
@@ -147,7 +147,7 @@ async def handle_commands(message: types.Message):
 
     if message.text and message.text.lower() == "таблица прокачки hamster":
         await message.answer(
-            "Ссылка на таблицу: [Google Sheets](https://docs.google.com/spreadsheets/d/1mVCjBu9-HfKn3l7UtoWAT37PDYoq757aQg0-lMCQe9w)",
+            "Ссылка на таблицу: [Прокачка Хомяка](https://docs.google.com/spreadsheets/d/1mVCjBu9-HfKn3l7UtoWAT37PDYoq757aQg0-lMCQe9w)",
             disable_web_page_preview=True,
             parse_mode="Markdown"
         )
